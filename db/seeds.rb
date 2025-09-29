889eff732957d127755f20ab3d989cb8b1ffec4b
douglas rocha <douglashidekazu15@gmail.com>
2025-09-29 15:46:56 +0900

    created seeds (accounts for owner and staff)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "creating and staff account..."
+
+User.create!(
+  name: "Douglas",
+  gender: "male",
+  email: "douglas@example.com",
+  password: "123456",
+  role: :owner,
+)
+
+User.create!(
+  name: "Giovanni",
+  gender: "male",
+  email: "giovanni@example.com",
+  password: "123456",
+  role: :staff,
+)
+
+puts "accounts cretaed successfully!"
